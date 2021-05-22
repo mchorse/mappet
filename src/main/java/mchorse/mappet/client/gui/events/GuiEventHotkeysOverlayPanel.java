7bcf3413c80d429a5b468ef9b432a3800b831fa0
McHorse <mchorselessrider@gmail.com>
2021-05-22 08:14:58 +0100

    Fix event hotkeys modal keys

--- src/main/java/mchorse/mappet/client/gui/events/GuiEventHotkeysOverlayPanel.java
+++ src/main/java/mchorse/mappet/client/gui/events/GuiEventHotkeysOverlayPanel.java
@@ -1,16 +1,21 @@
 package mchorse.mappet.client.gui.events;
 
+import mchorse.mappet.ClientProxy;
 import mchorse.mappet.api.events.hotkeys.EventHotkey;
 import mchorse.mappet.api.events.hotkeys.EventHotkeys;
+import mchorse.mappet.api.utils.ContentType;
 import mchorse.mappet.client.gui.utils.GuiCheckerElement;
 import mchorse.mappet.client.gui.utils.GuiMappetUtils;
+import mchorse.mappet.client.gui.utils.overlays.GuiContentNamesOverlayPanel;
+import mchorse.mappet.client.gui.utils.overlays.GuiOverlay;
 import mchorse.mappet.client.gui.utils.overlays.GuiOverlayPanel;
 import mchorse.mappet.network.Dispatcher;
 import mchorse.mappet.network.common.events.PacketEventHotkeys;
+import mchorse.mclib.client.gui.framework.GuiBase;
 import mchorse.mclib.client.gui.framework.elements.GuiScrollElement;
+import mchorse.mclib.client.gui.framework.elements.buttons.GuiButtonElement;
 import mchorse.mclib.client.gui.framework.elements.context.GuiSimpleContextMenu;
 import mchorse.mclib.client.gui.framework.elements.input.GuiKeybindElement;
-import mchorse.mclib.client.gui.framework.elements.input.GuiTextElement;
 import mchorse.mclib.client.gui.framework.elements.list.GuiListElement;
 import mchorse.mclib.client.gui.framework.elements.utils.GuiContext;
 import mchorse.mclib.client.gui.utils.Elements;
@@ -29,7 +34,7 @@ public class GuiEventHotkeysOverlayPanel extends GuiOverlayPanel
 
     public GuiScrollElement editor;
     public GuiKeybindElement key;
-    public GuiTextElement event;
+    public GuiButtonElement event;
     public GuiCheckerElement enabled;
 
     private EventHotkeys hotkeys;
@@ -37,7 +42,7 @@ public class GuiEventHotkeysOverlayPanel extends GuiOverlayPanel
 
     public GuiEventHotkeysOverlayPanel(Minecraft mc, EventHotkeys hotkeys)
     {
-        super(mc, IKey.lang("mappet.gui.nodes.event,hotkeys.title"));
+        super(mc, IKey.lang("mappet.gui.nodes.event.hotkeys.title"));
 
         this.hotkeys = hotkeys;
 
@@ -45,11 +50,11 @@ public class GuiEventHotkeysOverlayPanel extends GuiOverlayPanel
         this.list.sorting().setList(hotkeys.hotkeys);
         this.list.context(() ->
         {
-            GuiSimpleContextMenu menu = new GuiSimpleContextMenu(this.mc).action(Icons.ADD, IKey.lang("mappet.gui.nodes.event,hotkeys.context.add"), this::addHotkey);
+            GuiSimpleContextMenu menu = new GuiSimpleContextMenu(this.mc).action(Icons.ADD, IKey.lang("mappet.gui.nodes.event.hotkeys.context.add"), this::addHotkey);
 
             if (!this.hotkeys.hotkeys.isEmpty())
             {
-                menu.action(Icons.REMOVE, IKey.lang("mappet.gui.nodes.event,hotkeys.context.remove"), this::removeHotkey, 0xff0022);
+                menu.action(Icons.REMOVE, IKey.lang("mappet.gui.nodes.event.hotkeys.context.remove"), this::removeHotkey, 0xff0022);
             }
 
             return menu.shadow();
@@ -68,19 +73,30 @@ public class GuiEventHotkeysOverlayPanel extends GuiOverlayPanel
                 this.hotkey.keycode = k;
             }
         });
-        this.event = new GuiTextElement(mc, 1000, (t) -> this.hotkey.event = t);
+        this.event = new GuiButtonElement(mc, IKey.lang("mappet.gui.overlays.event"), (b) -> this.openEvents());
         this.enabled = new GuiCheckerElement(mc);
 
         this.list.flex().relative(this.content).w(120).h(1F);
         this.editor.flex().relative(this.content).x(120).w(1F, -120).h(1F).column(5).vertical().stretch().scroll().padding(10);
 
-        this.editor.add(Elements.label(IKey.lang("mappet.gui.nodes.event,hotkeys.key")), this.key);
-        this.editor.add(Elements.label(IKey.lang("mappet.gui.nodes.event,hotkeys.event")).marginTop(12), this.event);
-        this.editor.add(Elements.label(IKey.lang("mappet.gui.nodes.event,hotkeys.enabled")).marginTop(12), this.enabled);
+        this.editor.add(Elements.label(IKey.lang("mappet.gui.nodes.event.hotkeys.key")), this.key);
+        this.editor.add(Elements.label(IKey.lang("mappet.gui.nodes.event.hotkeys.event")).marginTop(12), this.event);
+        this.editor.add(Elements.label(IKey.lang("mappet.gui.nodes.event.hotkeys.enabled")).marginTop(12), this.enabled);
 
         this.content.add(this.list, this.editor);
 
         this.pickHotkey(hotkeys.hotkeys.isEmpty() ? null : hotkeys.hotkeys.get(0), true);
+    }
+
+    private void openEvents()
+    {
+        ClientProxy.requestNames(ContentType.EVENT, (names) ->
+        {
+            GuiContentNamesOverlayPanel overlay = new GuiContentNamesOverlayPanel(this.mc, IKey.lang("mappet.gui.overlays.event"), ContentType.EVENT, names, (id) -> this.hotkey.event = id);
+
+            overlay.set(this.hotkey.event);
+            GuiOverlay.addOverlay(GuiBase.getCurrent(), overlay, 0.4F, 0.6F);
+        });
     }
 
     private void addHotkey()
@@ -110,7 +126,6 @@ public class GuiEventHotkeysOverlayPanel extends GuiOverlayPanel
         if (hotkey != null)
         {
             this.key.setKeybind(hotkey.keycode);
-            this.event.setText(hotkey.event);
             this.enabled.set(hotkey.enabled);
 
             if (select)
