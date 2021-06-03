9883e13e50143f186d0e17eb6a4f4b94aac73e8a
McHorse <mchorselessrider@gmail.com>
2021-06-03 13:11:35 +0100

    Change `/mp quest decline` tab completion to current player’s quests instead of all quests

--- src/main/java/mchorse/mappet/commands/quests/CommandQuestDecline.java
+++ src/main/java/mchorse/mappet/commands/quests/CommandQuestDecline.java
@@ -6,6 +6,11 @@ import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
 
 public class CommandQuestDecline extends CommandQuestBase
 {
@@ -37,5 +42,26 @@ public class CommandQuestDecline extends CommandQuestBase
         {
             this.getL10n().success(sender, "quest.declined", args[1], player.getName());
         }
+    }
+
+    @Override
+    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos)
+    {
+        if (args.length == 2)
+        {
+            try
+            {
+                EntityPlayer player = getPlayer(server, sender, args[0]);
+                ICharacter character = Character.get(player);
+
+                return getListOfStringsMatchingLastWord(args, character.getQuests().quests.keySet());
+            }
+            catch (Exception e)
+            {}
+
+            return Collections.emptyList();
+        }
+
+        return super.getTabCompletions(server, sender, args, targetPos);
     }
 }
