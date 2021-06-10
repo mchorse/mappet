1f90bbf4afb641dabbb86f8a5aa9a7cd2f83597e
McHorse <mchorselessrider@gmail.com>
2021-06-10 12:35:39 +0100

    Add 'each' (seems to be Nashorn exclusive) keyword

--- src/main/java/mchorse/mappet/client/gui/scripts/utils/SyntaxHighlighter.java
+++ src/main/java/mchorse/mappet/client/gui/scripts/utils/SyntaxHighlighter.java
@@ -10,7 +10,7 @@ import java.util.Set;
 public class SyntaxHighlighter
 {
     private static final Set<String> OPERATORS = ImmutableSet.of("+", "-", "=", "/", "*", "<", ">", "~", "&", "|", "!");
-    private static final Set<String> PRIMARY_KEYWORDS = ImmutableSet.of("break", "continue", "switch", "case", "default", "try", "catch", "delete", "do", "while", "else", "finally", "if", "else", "for", "in", "instanceof", "new", "throw", "typeof", "with", "yield", "return");
+    private static final Set<String> PRIMARY_KEYWORDS = ImmutableSet.of("break", "continue", "switch", "case", "default", "try", "catch", "delete", "do", "while", "else", "finally", "if", "else", "for", "each", "in", "instanceof", "new", "throw", "typeof", "with", "yield", "return");
     private static final Set<String> SECONDARY_KEYWORDS = ImmutableSet.of("const", "function", "var", "let", "prototype", "Math", "JSON", "mappet");
     private static final Set<String> SPECIAL = ImmutableSet.of("this");
     private static final Set<String> TYPE_KEYSWORDS = ImmutableSet.of("true", "false", "null", "undefined");
