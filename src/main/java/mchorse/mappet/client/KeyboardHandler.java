93898aad0a24ca703ebbe438d52c3b78e10bc6ed
McHorse <mchorselessrider@gmail.com>
2021-07-23 18:21:45 +0100

    Add a button to open player's journal in inventories

--- src/main/java/mchorse/mappet/client/KeyboardHandler.java
+++ src/main/java/mchorse/mappet/client/KeyboardHandler.java
@@ -4,9 +4,20 @@ import mchorse.mappet.client.gui.GuiJournalScreen;
 import mchorse.mappet.client.gui.GuiMappetDashboard;
 import mchorse.mappet.network.Dispatcher;
 import mchorse.mappet.network.common.events.PacketEventHotkey;
+import mchorse.mclib.client.gui.framework.tooltips.styles.TooltipStyle;
+import mchorse.mclib.client.gui.utils.Area;
+import mchorse.mclib.client.gui.utils.keys.IKey;
+import mchorse.mclib.utils.Direction;
 import mchorse.mclib.utils.OpHelper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.inventory.GuiContainerCreative;
+import net.minecraft.client.gui.inventory.GuiInventory;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
@@ -30,6 +41,8 @@ public class KeyboardHandler
 
     public KeyBinding openMappetDashboard;
     public KeyBinding openJournal;
+
+    private GuiButton button;
 
     public KeyboardHandler()
     {
@@ -64,6 +77,61 @@ public class KeyboardHandler
             if (hotkeys.contains(key))
             {
                 Dispatcher.sendToServer(new PacketEventHotkey(key));
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onGuiInit(GuiScreenEvent.InitGuiEvent.Post event)
+    {
+        if (event.getGui() instanceof GuiInventory || event.getGui() instanceof GuiContainerCreative)
+        {
+            event.getButtonList().add(new GuiJournalButton(-69420, 0, event.getGui().height - 20, ""));
+        }
+    }
+
+    @SubscribeEvent
+    public void onGuiAction(GuiScreenEvent.ActionPerformedEvent.Post event)
+    {
+        if (event.getButton() instanceof GuiJournalButton)
+        {
+            Minecraft.getMinecraft().displayGuiScreen(new GuiJournalScreen(Minecraft.getMinecraft()));
+        }
+    }
+
+    public static class GuiJournalButton extends GuiButton
+    {
+        private static final ResourceLocation ICON = new ResourceLocation("textures/items/book_writable.png");
+        private static IKey TOOLTIP = IKey.lang("mappet.gui.player_journal");
+
+        private Area area = new Area();
+
+        public GuiJournalButton(int buttonId, int x, int y, String buttonText)
+        {
+            super(buttonId, x, y, 20, 20, buttonText);
+        }
+
+        @Override
+        public void drawButton(Minecraft mc, int mouseX, int mouseY, float partialTicks)
+        {
+            boolean hovered = mouseX >= this.x && mouseY >= this.y && mouseX < this.x + this.width && mouseY < this.y + this.height;
+            float factor = hovered ? 0.8F : 1F;
+
+            GlStateManager.enableTexture2D();
+            GlStateManager.color(factor, factor, factor, 1F);
+
+            mc.renderEngine.bindTexture(ICON);
+            Gui.drawModalRectWithCustomSizedTexture(this.x + this.width / 2 - 8, this.y + this.height / 2 - 8, 0, 0, 16, 16, 16, 16);
+
+            if (hovered)
+            {
+                String label = TOOLTIP.get();
+                TooltipStyle style = TooltipStyle.get();
+                int w = mc.fontRenderer.getStringWidth(label);
+
+                this.area.set(this.x + 20, this.y + this.height / 2 - 7, w + 6, 14);
+                style.drawBackground(this.area);
+                mc.fontRenderer.drawString(label, this.area.x + 3, this.area.y + 3, style.getTextColor());
             }
         }
     }
