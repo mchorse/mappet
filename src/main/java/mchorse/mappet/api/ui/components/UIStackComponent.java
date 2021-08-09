8002255d626c844b22d0dac0dc6833608ee1644c
McHorse <mchorselessrider@gmail.com>
2021-08-09 20:32:28 +0100

    Document item stack UI component

--- src/main/java/mchorse/mappet/api/ui/components/UIStackComponent.java
+++ src/main/java/mchorse/mappet/api/ui/components/UIStackComponent.java
@@ -1,6 +1,7 @@
 package mchorse.mappet.api.ui.components;
 
 import mchorse.mappet.api.scripts.user.items.IScriptItemStack;
+import mchorse.mappet.api.scripts.user.mappet.IMappetUIBuilder;
 import mchorse.mappet.api.ui.UIContext;
 import mchorse.mappet.api.ui.utils.DiscardMethod;
 import mchorse.mclib.client.gui.framework.elements.GuiElement;
@@ -11,15 +12,66 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+/**
+ * Item stack (slot) UI component.
+ *
+ * <p>This component allows users to input an item stack. The value that gets written
+ * to UI context's data (if ID is present) is an NBT compound tag.</p>
+ *
+ * <p>This component can be created using {@link IMappetUIBuilder#item()} method.</p>
+ *
+ * <pre>{@code
+ *    function main(c)
+ *    {
+ *        var ui = mappet.createUI(c, "handler").background();
+ *        var stack = ui.item().id("stack").tooltip("To dupe an item, please pick that item.");
+ *
+ *        stack.rxy(0.5, 0.5).wh(24, 24).anchor(0.5);
+ *        c.getSubject().openUI(ui);
+ *    }
+ *
+ *    function handler(c)
+ *    {
+ *        var uiContext = c.getSubject().getUIContext();
+ *        var data = uiContext.getData();
+ *
+ *        if (uiContext.getLast() === "stack")
+ *        {
+ *            var item = mappet.createItem(data.getCompound("stack"));
+ *            var pos = c.getSubject().getPosition();
+ *
+ *            c.getWorld().dropItemStack(item, pos.x, pos.y + 2, pos.z);
+ *        }
+ *    }
+ * }</pre>
+ */
 public class UIStackComponent extends UIComponent
 {
     public ItemStack stack = ItemStack.EMPTY;
 
+    /**
+     * Set item stack component's item from scripts.
+     *
+     * <pre>{@code
+     *    function main(c)
+     *    {
+     *        var ui = mappet.createUI().background();
+     *        var stack = ui.item().id("stack").tooltip("An exhibit D.", 1);
+     *
+     *        stack.rxy(0.5, 0.5).wh(24, 24).anchor(0.5);
+     *        stack.stack(mappet.createItem("minecraft:diamond_sword"));
+     *        c.getSubject().openUI(ui);
+     *    }
+     * }</pre>
+     */
     public UIStackComponent stack(IScriptItemStack stack)
     {
         return this.stack(stack == null ? null : stack.getMinecraftItemStack());
     }
 
+    /**
+     * Set item stack component's item. See the example in {@link #stack(IScriptItemStack)}.
+     */
     public UIStackComponent stack(ItemStack stack)
     {
         this.change("Stack");
