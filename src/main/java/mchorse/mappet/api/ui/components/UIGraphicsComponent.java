e9cf3a6aa6b5a3076b54400edc3b09e92de15acb
McHorse <mchorselessrider@gmail.com>
2021-08-10 20:22:03 +0100

    Document graphics UI component

--- src/main/java/mchorse/mappet/api/ui/components/UIGraphicsComponent.java
+++ src/main/java/mchorse/mappet/api/ui/components/UIGraphicsComponent.java
@@ -1,5 +1,6 @@
 package mchorse.mappet.api.ui.components;
 
+import mchorse.mappet.api.scripts.user.mappet.IMappetUIBuilder;
 import mchorse.mappet.api.ui.UIContext;
 import mchorse.mappet.api.ui.utils.DiscardMethod;
 import mchorse.mappet.client.gui.utils.GuiGraphics;
@@ -21,6 +22,61 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Graphics UI component.
+ *
+ * <p>This component allows drawing solid colored rectangles, gradient rectangles,
+ * images loaded through Minecraft's texture manager, text and McLib icons. Think
+ * of it as a very primitive canvas implementation.</p>
+ *
+ * <p>This component can be created using {@link IMappetUIBuilder#graphics()} method.</p>
+ *
+ * <pre>{@code
+ *    function main(c)
+ *    {
+ *        var ui = mappet.createUI(c, "handler").background();
+ *
+ *        // Background rendering
+ *        var back = ui.graphics().rx(0.5, -150).ry(1, -250).wh(300, 250);
+ *        var icons = Java.type("mchorse.mclib.client.gui.utils.IconRegistry").icons.keySet();
+ *
+ *        for each (var icon in icons)
+ *        {
+ *            var x = Math.random() * 280 + 10;
+ *            var y = Math.random() * 230 + 10;
+ *
+ *            back.icon(icon, x - 1, y - 1, 0xff000000 + Math.random() * 0xffffff);
+ *        }
+ *
+ *        // Draw my favorite "me"
+ *        var m = mappet.createMorph("{Skin:\"blockbuster:textures/entity/mchorse/skin.png\",CustomPose:{Size:[0.6f,1.8f,0.6f],Poses:{right_arm:{P:[-6.0f,-2.0f,0.0f],R:[-90.0f,-31.0f,0.0f]},left_leg:{P:[2.0f,-12.0f,0.0f]},right_armwear:{P:[0.0f,-4.0f,0.0f]},outer:{P:[0.0f,4.0f,0.0f]},left_legwear:{P:[0.0f,-6.0f,0.0f]},body:{P:[0.0f,8.0f,0.0f],R:[0.0f,-29.0f,0.0f]},bodywear:{P:[0.0f,-6.0f,0.0f]},head:{P:[0.0f,8.0f,0.0f],R:[7.0f,-51.0f,0.0f]},left_arm:{P:[6.0f,-2.0f,0.0f]},right_leg:{P:[-2.0f,-12.0f,0.0f]},right_legwear:{P:[0.0f,-6.0f,0.0f]},anchor:{P:[0.0f,16.0f,0.0f]},left_armwear:{P:[0.0f,-4.0f,0.0f]}}},BodyParts:[{Limb:\"head\",Morph:{Settings:{Hands:1b},Name:\"blockbuster.mchorse/head\"}}],Settings:{Hands:1b},Name:\"blockbuster.fred_3d\"}");
+ *        var morph = ui.morph(m).id("icon");
+ *
+ *        morph.rx(0.5, -150).ry(1, -250).wh(300, 250).position(-0.017, 1.367, 0).rotation(0, 0).distance(2.2).fov(40).enabled(false);
+ *
+ *        // Draw foreground
+ *        var graphics = ui.graphics().rx(0.5, -150).ry(1, -250).wh(300, 250);
+ *
+ *        // Draw small rectangles
+ *        for (var i = 0; i < 100; i++)
+ *        {
+ *            var x = Math.random() * 280 + 10;
+ *            var y = Math.random() * 230 + 10;
+ *
+ *            graphics.rect(x - 1, y - 1, 2, 2, 0x88000000 + Math.random() * 0xffffff);
+ *        }
+ *
+ *        graphics.gradient(0, 210, 300, 40, 0x00ff0000, 0xffff0000);
+ *        // This is Thanos' infinity gauntlet. just sayin in case it gets removed xd
+ *        graphics.image("https://i.pinimg.com/originals/6d/5a/99/6d5a99fa0f01dff1dd5de5e3b8244f8d.png", 0, 75, 55, 100);
+ *        graphics.text("McThanos", 120, 230, 0xffffff);
+ *
+ *        ui.label("Graphic Design is my passion").color(0x00ff00).background(0x88000000).rxy(0.5, 0.25).wh(100, 20).anchor(0.5).labelAnchor(0.5);
+ *
+ *        c.getSubject().openUI(ui);
+ *    }
+ * }</pre>
+ */
 public class UIGraphicsComponent extends UIComponent
 {
     public List<Graphic> graphics = new ArrayList<Graphic>();
@@ -33,51 +89,118 @@ public class UIGraphicsComponent extends UIComponent
         return this;
     }
 
+    /**
+     * Draw a solid colored rectangle relative to graphics component's frame.
+     *
+     * @param w Width of the rectangle.
+     * @param h Height of the rectangle.
+     * @param color ARGB color that fills the rectangle.
+     */
     public UIGraphicsComponent rect(int x, int y, int w, int h, int color)
     {
         return this.addGraphic(new RectGraphic(x, y, w, h, color));
     }
 
+    /**
+     * Draw a vertical gradient rectangle relative to graphics component's frame.
+     *
+     * @param w Width of the rectangle.
+     * @param h Height of the rectangle.
+     * @param primary ARGB color that fills top part of the gradient.
+     * @param secondary ARGB color that fills bottom part of the gradient.
+     */
     public UIGraphicsComponent gradient(int x, int y, int w, int h, int primary, int secondary)
     {
         return this.gradient(x, y, w, h, primary, secondary, false);
     }
 
+    /**
+     * Draw a gradient rectangle relative to graphics component's frame.
+     *
+     * @param w Width of the rectangle.
+     * @param h Height of the rectangle.
+     * @param primary ARGB color that fills top or left part of the gradient.
+     * @param secondary ARGB color that fills bottom or right part of the gradient.
+     * @param horizontal Whether gradient is horizontal (<code>true</code>) or vertical (<code>false</code>).
+     */
     public UIGraphicsComponent gradient(int x, int y, int w, int h, int primary, int secondary, boolean horizontal)
     {
         return this.addGraphic(new GradientGraphic(x, y, w, h, primary, secondary, horizontal));
     }
 
+    /**
+     * Draw an image relative to graphics component's frame.
+     *
+     * <p>Image argument is a so called "resource location." For example, if you want
+     * to draw pig's skin on the screen you can input "minecraft:textures/entity/pig/pig.png"
+     * and it will draw it on the screen.</p>
+     *
+     * <p>If you have Blockbuster enabled, it can also display images from internet
+     * by inputting image's URL. Although, sometimes it won't work due to incorrect headers
+     * that doesn't identify a web-browser.</p>
+     */
     public UIGraphicsComponent image(String image, int x, int y, int w, int h)
     {
         return this.image(image, x, y, w, h, w, h, 0xffffffff);
     }
 
+    /**
+     * Draw an image relative to graphics component's frame with known texture size.
+     */
     public UIGraphicsComponent image(String image, int x, int y, int w, int h, int textureWidth, int textureHeight)
     {
         return this.image(image, x, y, w, h, textureWidth, textureHeight, 0xffffffff);
     }
 
+    /**
+     * Draw an image relative to graphics component's frame with known texture size and color.
+     */
     public UIGraphicsComponent image(String image, int x, int y, int w, int h, int textureWidth, int textureHeight, int primary)
     {
         return this.addGraphic(new ImageGraphic(new ResourceLocation(image), x, y, w, h, textureWidth, textureHeight, primary));
     }
 
+    /**
+     * Draw a text label relative to graphics component's frame.
+     *
+     * @param color ARGB text's font color.
+     */
     public UIGraphicsComponent text(String text, int x, int y, int color)
     {
         return this.text(text, x, y, color, 0, 0);
     }
 
+    /**
+     * Draw a text label with an anchor relative to graphics component's frame.
+     *
+     * @param color ARGB text's font color.
+     * @param anchorX Horizontal anchor (<code>0..1</code>).
+     * @param anchorY Vertical anchor (<code>0..1</code>).
+     */
     public UIGraphicsComponent text(String text, int x, int y, int color, float anchorX, float anchorY)
     {
         return this.addGraphic(new TextGraphic(text, x, y, color, anchorX, anchorY));
     }
 
+    /**
+     * Draw a McLib icon relative to graphics component's frame.
+     *
+     * <p>Most of the icons are <code>16x16</code> so keep that in mind.</p>
+     *
+     * @param color ARGB color that used to render an icon.
+     */
     public UIGraphicsComponent icon(String icon, int x, int y, int color)
     {
         return this.icon(icon, x, y, color, 0, 0);
     }
 
+    /**
+     * Draw a McLib icon with an anchor relative to graphics component's frame.
+     *
+     * @param color ARGB color that used to render an icon.
+     * @param anchorX Horizontal anchor (<code>0..1</code>).
+     * @param anchorY Vertical anchor (<code>0..1</code>).
+     */
     public UIGraphicsComponent icon(String icon, int x, int y, int color, float anchorX, float anchorY)
     {
         return this.addGraphic(new IconGraphic(icon, x, y, color, anchorX, anchorY));
