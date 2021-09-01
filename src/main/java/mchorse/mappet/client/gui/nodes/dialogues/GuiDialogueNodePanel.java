027c94023f8a637983af6d0b93ac5085d648d6c7
McHorse <mchorselessrider@gmail.com>
2021-09-01 20:50:40 +0100

    Add multi line field to reply and reaction

--- src/main/java/mchorse/mappet/client/gui/nodes/dialogues/GuiDialogueNodePanel.java
+++ src/main/java/mchorse/mappet/client/gui/nodes/dialogues/GuiDialogueNodePanel.java
@@ -3,6 +3,7 @@ package mchorse.mappet.client.gui.nodes.dialogues;
 import mchorse.mappet.api.dialogues.nodes.DialogueNode;
 import mchorse.mappet.client.gui.nodes.GuiEventBaseNodePanel;
 import mchorse.mappet.client.gui.utils.GuiMappetUtils;
+import mchorse.mappet.client.gui.utils.text.GuiMultiTextElement;
 import mchorse.mclib.client.gui.framework.elements.input.GuiColorElement;
 import mchorse.mclib.client.gui.framework.elements.input.GuiTextElement;
 import mchorse.mclib.client.gui.utils.Elements;
@@ -11,17 +12,15 @@ import net.minecraft.client.Minecraft;
 
 public class GuiDialogueNodePanel extends GuiEventBaseNodePanel<DialogueNode>
 {
-    public GuiTextElement text;
+    public GuiMultiTextElement text;
     public GuiColorElement color;
 
     public GuiDialogueNodePanel(Minecraft mc)
     {
         super(mc);
 
-        this.text = GuiMappetUtils.fullWindowContext(
-            new GuiTextElement(mc, 10000, (text) -> this.node.message.text = text),
-            IKey.lang("mappet.gui.nodes.dialogue.content")
-        );
+        this.text = new GuiMultiTextElement(mc, (text) -> this.node.message.text = text);
+        this.text.background().flex().h(100);
         this.color = new GuiColorElement(mc, (c) -> this.node.message.color = c);
 
         this.add(Elements.label(IKey.lang("mappet.gui.nodes.dialogue.content")).marginTop(12), this.text, this.color);
