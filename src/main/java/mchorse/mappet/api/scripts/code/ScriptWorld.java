ecdb9fd3fa02d3947427cf210cd7b23e895ce691
OtakuGamerAds <Otakugamerads@gmail.com>
2022-12-13 23:13:00 +0100

    Added in ScriptWorld: new methods.

--- src/main/java/mchorse/mappet/api/scripts/code/ScriptWorld.java
+++ src/main/java/mchorse/mappet/api/scripts/code/ScriptWorld.java
@@ -1,6 +1,8 @@
 package mchorse.mappet.api.scripts.code;
 
 import io.netty.buffer.Unpooled;
+import mchorse.blockbuster.common.tileentity.TileEntityModel;
+import mchorse.blockbuster.network.common.PacketModifyModelBlock;
 import mchorse.mappet.Mappet;
 import mchorse.mappet.api.npcs.Npc;
 import mchorse.mappet.api.npcs.NpcState;
@@ -13,6 +15,7 @@ import mchorse.mappet.api.scripts.user.IScriptRayTrace;
 import mchorse.mappet.api.scripts.user.IScriptWorld;
 import mchorse.mappet.api.scripts.user.blocks.IScriptBlockState;
 import mchorse.mappet.api.scripts.user.blocks.IScriptTileEntity;
+import mchorse.mappet.api.scripts.user.data.ScriptVector;
 import mchorse.mappet.api.scripts.user.entities.IScriptEntity;
 import mchorse.mappet.api.scripts.user.entities.IScriptNpc;
 import mchorse.mappet.api.scripts.user.entities.IScriptPlayer;
@@ -27,15 +30,21 @@ import mchorse.mappet.network.common.scripts.PacketWorldMorph;
 import mchorse.mappet.utils.WorldUtils;
 import mchorse.mclib.utils.MathUtils;
 import mchorse.metamorph.api.morphs.AbstractMorph;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockButton;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Blocks;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.play.server.SPacketCustomPayload;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -43,6 +52,7 @@ import net.minecraft.world.WorldServer;
 import net.minecraft.world.chunk.storage.AnvilChunkLoader;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -405,5 +415,189 @@ public class ScriptWorld implements IScriptWorld
         NetworkRegistry.TargetPoint point = new NetworkRegistry.TargetPoint(dimension, x, y, z, MathUtils.clamp(range, 1, 256));
 
         Dispatcher.DISPATCHER.get().sendToAllAround(new PacketWorldMorph(worldMorph), point);
+    }
+
+    @Override
+    public void setModelBlock(String nbt, int x, int y, int z, boolean force)
+    {
+        if (!this.world.isBlockLoaded(this.pos.setPos(x, y, z)))
+        {
+            return;
+        }
+        //if the modelBlock already has the same nbt, don't do anything
+        IScriptTileEntity te = getTileEntity(x, y, z);
+        if (te != null ) {
+            INBTCompound teCompound = te.getData().getCompound("Morph");
+            ScriptFactory factory = new ScriptFactory();
+            INBTCompound nbtCompound = factory.createCompound(nbt);
+            if ( teCompound.equals(nbtCompound) ){
+                if (!force) {
+                    return;
+                }
+            }
+        }
+        ScriptFactory factory = new ScriptFactory();
+        setBlock(factory.createBlockState("minecraft:air",0), x, y, z);
+        setBlock(factory.createBlockState("blockbuster:model",0), x, y, z);
+        TileEntityModel model = (TileEntityModel) this.world.getTileEntity(new BlockPos(x, y, z));
+        TileEntity tile = this.world.getTileEntity(this.pos.setPos(x, y, z));
+        ScriptFactory scriptFactory = new ScriptFactory();
+        model.setMorph(factory.createMorph(nbt));
+        PacketModifyModelBlock message = new PacketModifyModelBlock(model.getPos(), model, true);
+        mchorse.blockbuster.network.Dispatcher.DISPATCHER.get().sendToAllAround(message, new NetworkRegistry.TargetPoint(this.world.provider.getDimension(), model.getPos().getX(), model.getPos().getY(), model.getPos().getZ(), 64));
+    }
+
+    @Override
+    public boolean isActive(int x, int y, int z){
+        return this.world.getBlockState(this.pos.setPos(x, y, z)).getValue(BlockButton.POWERED);
+    }
+
+    @Override
+    public boolean testForBlock(int x, int y, int z, String blockId, int meta){
+        return getBlock(x, y, z).isSame(ScriptBlockState.create(ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockId)).getStateFromMeta(meta)));
+    }
+
+    @Override
+    public void fill(IScriptBlockState state, int x1, int y1, int z1, int x2, int y2, int z2){
+        int xMin = Math.min(x1, x2);
+        int xMax = Math.max(x1, x2);
+        int yMin = Math.min(y1, y2);
+        int yMax = Math.max(y1, y2);
+        int zMin = Math.min(z1, z2);
+        int zMax = Math.max(z1, z2);
+        for(int x = xMin; x <= xMax; x++){
+            for(int y = yMin; y <= yMax; y++){
+                for(int z = zMin; z <= zMax; z++){
+                    setBlock(state, x, y, z);
+                }
+            }
+        }
+    }
+
+    @Override
+    public IScriptEntity summonFallingBlock(int x, int y, int z, String blockId, int meta){
+        ScriptFactory factory = new ScriptFactory();
+        INBTCompound nbt = factory.createCompound();
+        nbt.setString("Block", blockId);
+        nbt.setInt("Data", meta);
+        nbt.setInt("Time", 1);
+        return spawnEntity("minecraft:falling_block", x, y, z, nbt);
+    }
+
+    @Override
+    public IScriptEntity setFallingBlock(int x, int y, int z){
+        IScriptBlockState state = getBlock(x, y, z);
+        if(state.getBlockId().equals("minecraft:air")){return null;}
+        ScriptFactory factory = new ScriptFactory();
+        INBTCompound nbt = factory.createCompound();
+        nbt.setString("Block", state.getBlockId());
+        nbt.setInt("Data", state.getMeta());
+        nbt.setInt("Time", 1);
+
+        Block block = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(state.getBlockId()));
+        if( block != null){
+            IBlockState blockState = block.getStateFromMeta(state.getMeta());
+            for (IProperty<?> property : blockState.getProperties().keySet()) {
+                nbt.setString(property.getName(), blockState.getValue(property).toString());
+            }
+        }
+
+        if (hasTileEntity(x, y, z)) {
+            nbt.setCompound("TileEntityData", getTileEntity(x, y, z).getData());
+        }
+
+        this.world.removeTileEntity(this.pos.setPos(x, y, z));
+        this.world.setBlockState(this.pos.setPos(x, y, z), Blocks.AIR.getDefaultState(), 3);
+
+        IScriptEntity entity = spawnEntity("minecraft:falling_block", x+0.5, y+0.5, z+0.5, nbt);
+        return entity;
+    }
+
+
+    @Override
+    public List<IScriptEntity> fancyExplode(int x1, int y1, int z1, int x2, int y2, int z2, int blocksPercentage){
+        List<IScriptEntity> entities = new ArrayList<IScriptEntity>();
+        int xMin = Math.min(x1, x2);
+        int xMax = Math.max(x1, x2);
+        int yMin = Math.min(y1, y2);
+        int yMax = Math.max(y1, y2);
+        int zMin = Math.min(z1, z2);
+        int zMax = Math.max(z1, z2);
+        int xCentre = (xMin+xMax)/2;
+        int yCentre = (yMin+yMax)/2;
+        int zCentre = (zMin+zMax)/2;
+
+        playSound("minecraft:entity.generic.explode", xCentre, yCentre, zCentre, 1, 1);
+
+        for (int x = xMin; x <= xMax; x++) {
+            for (int y = yMin; y <= yMax; y++) {
+                for (int z = zMin; z <= zMax; z++) {
+                    //if the blocks are closer the centre, they are more likely to be destroyed
+                    if (Math.random() < (1 - (Math.sqrt(Math.pow(xCentre - x, 2) + Math.pow(yCentre - y, 2) + Math.pow(zCentre - z, 2)) / Math.sqrt(Math.pow(xCentre - xMin, 2) + Math.pow(yCentre - yMin, 2) + Math.pow(zCentre - zMin, 2)))) * blocksPercentage / 100) {
+                        IScriptEntity entity = setFallingBlock(x, y, z);
+                        if(entity != null){
+                            entity.addVelocity((x-xCentre)/2.0, (y-yCentre)/2.0, (z-zCentre)/2.0);
+                            entities.add(entity);
+
+                            //particles
+                            ScriptVector pos = entity.getPosition();
+                            ScriptFactory factory = new ScriptFactory();
+                            EnumParticleTypes particle = factory.getParticleType("explode");
+                            spawnParticles(particle, true, pos.x, pos.y, pos.z, 10, 1, 1, 1, 0.1);
+                        }
+                    }
+                }
+            }
+        }
+        return entities;
+    }
+
+    @Override
+    public List<IScriptEntity> fancyExplode(int x, int y, int z, int radius, int blocksPercentage){
+        return fancyExplode(x-radius, y-radius, z-radius, x+radius, y+radius, z+radius, blocksPercentage);
+    }
+
+    @Override
+    public void tpExplode(int x1, int y1, int z1, int x2, int y2, int z2, int blocksPercentage) {
+        int xMin = Math.min(x1, x2);
+        int xMax = Math.max(x1, x2);
+        int yMin = Math.min(y1, y2);
+        int yMax = Math.max(y1, y2);
+        int zMin = Math.min(z1, z2);
+        int zMax = Math.max(z1, z2);
+        int xCentre = (xMin + xMax) / 2;
+        int yCentre = (yMin + yMax) / 2;
+        int zCentre = (zMin + zMax) / 2;
+
+        playSound("minecraft:entity.generic.explode", xCentre, yCentre, zCentre, 1, 1);
+
+        for (int x = xMin; x <= xMax; x++) {
+            for (int y = yMin; y <= yMax; y++) {
+                for (int z = zMin; z <= zMax; z++) {
+                    //if the blocks are closer the centre, they are more likely to be destroyed
+                    if (Math.random() < (1 - (Math.sqrt(Math.pow(xCentre - x, 2) + Math.pow(yCentre - y, 2) + Math.pow(zCentre - z, 2)) / Math.sqrt(Math.pow(xCentre - xMin, 2) + Math.pow(yCentre - yMin, 2) + Math.pow(zCentre - zMin, 2)))) * blocksPercentage / 100) {
+                        IScriptBlockState state = getBlock(x, y, z);
+                        if (!state.getBlockId().equals("minecraft:air")) {
+                            setBlock(ScriptBlockState.create(Blocks.AIR.getDefaultState()), x, y, z);
+                            //place the removed blocks randomly far away from the centre (but not far)
+                            int xNew = (int) (Math.random() * (xMax - xMin) + xMin);
+                            int yNew = (int) (Math.random() * (yMax - yMin) + yMin);
+                            int zNew = (int) (Math.random() * (zMax - zMin) + zMin);
+                            setBlock(state, xCentre + (int) (Math.random() * 10 - 5), yCentre + (int) (Math.random() * 10 - 5), zCentre + (int) (Math.random() * 10 - 5));
+
+                            //particles
+                            ScriptFactory factory = new ScriptFactory();
+                            EnumParticleTypes particle = factory.getParticleType("explode");
+                            spawnParticles(particle, true, xNew + 0.5, yNew + 0.5, zNew + 0.5, 10, 1, 1, 1, 0.1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void tpExplode(int x, int y, int z, int radius, int blocksPercentage) {
+        tpExplode(x - radius, y - radius, z - radius, x + radius, y + radius, z + radius, blocksPercentage);
     }
 }
