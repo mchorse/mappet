e2a700af1fa6aeea81aee445326baae8bdd8276e
OtakuGamerAds <Otakugamerads@gmail.com>
2023-03-28 17:40:04 +0200

    support selectors in `npc follow` (you can put i.e. @e[mpid=npcname])

---
+++ src/main/java/mchorse/mappet/entities/CommandNpc.java
@@ -0,0 +1,74 @@
+package mchorse.mappet.entities;
+
+import net.minecraft.command.ICommandManager;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+
+public class CommandNpc {
+    public String command = "";
+
+    public CommandNpc()
+    {}
+
+    public CommandNpc(String command)
+    {
+        this.command = command;
+    }
+
+    public void apply(EntityNpc npc)
+    {
+        if (!this.command.isEmpty())
+        {
+            MinecraftServer server = npc.world.getMinecraftServer();
+            if (server != null)
+            {
+                ICommandManager manager = server.commandManager;
+                manager.executeCommand(new CommandSender(npc), this.command);
+            }
+        }
+    }
+
+    public static ICommandSender getCommandSender(EntityNpc npc)
+    {
+        return new CommandSender(npc);
+    }
+
+    public static class CommandSender implements ICommandSender
+    {
+        public EntityNpc npc;
+
+        public CommandSender(EntityNpc npc)
+        {
+            this.npc = npc;
+        }
+
+        @Override
+        public String getName()
+        {
+            return "CommandNPC";
+        }
+
+        @Override
+        public boolean canUseCommand(int permLevel, String commandName)
+        {
+            return true;
+        }
+
+        @Override
+        public World getEntityWorld()
+        {
+            return this.npc.world;
+        }
+
+        @Nullable
+        @Override
+        public MinecraftServer getServer()
+        {
+            return this.npc.world.getMinecraftServer();
+        }
+
+    }
+}
